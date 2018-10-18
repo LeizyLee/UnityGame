1cd4a6b2303a46636ac6c2949498dce11ecf718d
LeizyLee <hwansang5@gmail.com>
2018-10-18 12:57:38 +0900

    asd
    asd

---
+++ Roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    public Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+	void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, movevertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
